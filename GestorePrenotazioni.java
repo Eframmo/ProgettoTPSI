b35d6a651fc9de931bd5a36e7ec6b99802b4aacc
Eframmo <hanoun.efraim@studenti.salesianisesto.it>
2026-02-15 14:44:51 +0100

    Fatto metodo: prenota()

--- GestorePrenotazioni.java
+++ GestorePrenotazioni.java
@@ -8,7 +8,25 @@ public class GestorePrenotazioni {
 
     }
 
-    public void prenota(String nomeCliente, int numeroPersone, String tipologia, int ore){
+    public void prenota(String nomeCliente, int numeroPersone, String tipologia, int ore) throws StanzaOccupataException {
+        if (nomeCliente == null){
+            throw new IllegalArgumentException("Nome non valido");
+        }
 
+        for (int i = 0; i < stanze.size(); i++){
+
+            if (tipologia.equals("Desk") && stanze.get(i) instanceof Desk){
+                stanze.get(i).assegna();
+                break;
+            }else if (tipologia.equals("Meeting Room") && stanze.get(i) instanceof MeetingRoom){
+                stanze.get(i).assegna();
+                break;
+            }else if (tipologia.equals("Private Office") && stanze.get(i) instanceof PrivateOffice){
+                stanze.get(i).assegna();
+                break;
+            }else {
+                throw new IllegalArgumentException("Stanza non riconosciuta");
+            }
+        }
     }
 }
